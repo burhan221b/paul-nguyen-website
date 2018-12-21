ee3331c83355eb4feb6f6e955c813358f16ef8be
Burhan <burhan221b@gmail.com>
2018-12-21 03:01:32 -0800

    initial commit

---
+++ Components/footer.php
@@ -0,0 +1,36 @@
+ <!-- footer -->
+ <section class="five">
+        <div class="container-fluid">
+            <div class="content">
+                <div class="row">
+                    <div id='imgs' class="col-lg-4 col-md-4 col-sm-12">
+                        <img id='footerlogo' src="./Images/logo/logo.png" alt="Paul Nguyen Automotive Repair Service" />
+                        <div class="main-img-two">
+                            <img src="./Images/logo/SMOGLOGO.jpg" alt="Smog Check icon">
+                            <img src="./Images/logo/ase.jpg" alt="Master ASE Certified icon">
+                        </div>
+                    </div>
+                    <div class="text col-lg-4 col-md-4 col-sm-12">
+                        <h3>Location</h3>
+                        <p>498 N. Mathilda Avenue,</p>
+                        <p>Sunnyvale, CA 94085</p>
+                        <p>Phone: (408) 245 - 2122</p>
+                        <p>Fax:(408) 245 - 1211</p>
+                    </div>
+                    <div class="text col-lg-4 col-md-4 col-sm-12">
+                        <h3>Hours</h3>
+                        <p>Open 7 Days:</p>
+                        <p>8:30 AM - 6 PM</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <footer id='end'>
+                <small>Copyright &copy; <?php echo date('Y')?>, Proudly created by Burhan.</small>
+    </footer>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,92 @@
+<?php include './Components/header.php';?>
+
+
+    <div class="clearfix"></div>
+    <!-- Banner -->
+    <section class='one'>
+        <div class="container-fluid">
+            <div class="content">
+                <h2>Paul Nguyen</h2>
+                <h3>Service Manager - Master ASE Certified</h3>
+                <h3>Complete Auto Repair - Licensed Smog Check Station </h3>
+            </div>
+        </div>
+    </section>
+    <!-- Services -->
+    <section class='two'>
+        <div class="container-fluid">
+            <div class="content">
+                <div class="header">
+                <h2>Services</h2>
+                <div class="main-img-two">
+                    <img src="./Images/logo/SMOGLOGO.jpg" alt="Smog Check icon">
+                </div></div>
+                <div class="lists">
+                <div class="list">
+                    <ul>
+                        <li>Smog Check & Testing</li>
+                        <li>Oil Change</li>
+                        <li>Tires Check, Removal, & Installation</li>
+                        <li>Transmission Repair</li>
+                        <li>Battery Replacement </li>
+                    </ul>
+
+                </div>
+                <div class="list">
+                <ul>
+                        <li>Brake Check & Replacement</li>
+                        <li>Tune Ups</li>
+                        <li>Wheel Alignment</li>
+                        <li>Belt Alignment & Repair</li>
+                    </ul>
+                    <p class='learnmore'><a>Learn more about our Services</a></p>
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Yelp Reviews Section -->
+    <section class='three'>
+        <div class="container-fluid">
+            <div class="content">
+            <h2>Yelp Reivews</h2>
+                <!-- <ul id="reviewers"> -->
+            <div class="row">
+            <div class="shadeone col-lg-12 col-xl-4">
+            <span class="yelp-review" data-review-id="Pn5UpP0rbGcpqjhzG9KSyA" data-hostname="www.yelp.com">Read <a href="https://www.yelp.com/user_details?userid=unj23l3n1yQWoOWFWZRZTQ" rel="nofollow noopener">Kiwi W.</a>'s <a href="https://www.yelp.com/biz/mathilda-valero-station-auto-services-sunnyvale?hrid=Pn5UpP0rbGcpqjhzG9KSyA" rel="nofollow noopener">review</a> of <a href="https://www.yelp.com/biz/Dx0iRSIpLOrjFVfsR1WEDA" rel="nofollow noopener">Mathilda Valero Station Auto Services</a> on <a href="https://www.yelp.com" rel="nofollow noopener">Yelp</a><script async="async" src="https://www.yelp.com/embed/widgets.js" type="text/javascript"></script></span>
+
+            </div>
+            <div class="shadetwo col-lg-12 col-xl-4">
+            <span class="yelp-review" data-review-id="IlGxYGG5Ug60ZAQUKePTGA" data-hostname="www.yelp.com">Read <a href="https://www.yelp.com/user_details?userid=9dSRCbDGGlwk8jj3Vo9ODg" rel="nofollow noopener">Nakata D.</a>'s <a href="https://www.yelp.com/biz/mathilda-valero-station-auto-services-sunnyvale?hrid=IlGxYGG5Ug60ZAQUKePTGA" rel="nofollow noopener">review</a> of <a href="https://www.yelp.com/biz/Dx0iRSIpLOrjFVfsR1WEDA" rel="nofollow noopener">Mathilda Valero Station Auto Services</a> on <a href="https://www.yelp.com" rel="nofollow noopener">Yelp</a><script async="async" src="https://www.yelp.com/embed/widgets.js" type="text/javascript"></script></span>
+
+            </div>
+            <div class="shadethree col-lg-12 col-xl-4">
+            <span class="yelp-review" data-review-id="7fzvSWILXmmmJTwbLmCcnQ" data-hostname="www.yelp.com">Read <a href="https://www.yelp.com/user_details?userid=RIO03aTd0J35uzYINR742A" rel="nofollow noopener">Linda H.</a>'s <a href="https://www.yelp.com/biz/mathilda-valero-station-auto-services-sunnyvale?hrid=7fzvSWILXmmmJTwbLmCcnQ" rel="nofollow noopener">review</a> of <a href="https://www.yelp.com/biz/Dx0iRSIpLOrjFVfsR1WEDA" rel="nofollow noopener">Mathilda Valero Station Auto Services</a> on <a href="https://www.yelp.com" rel="nofollow noopener">Yelp</a><script async="async" src="https://www.yelp.com/embed/widgets.js" type="text/javascript"></script></span> 
+
+            </div>
+            </div>
+        </div>
+    </section>
+    <!-- Location and Time -->
+    <section class='four'>
+        <div class="container-fluid">
+            <div class="content">
+            <h2>Location</h2>
+
+                <div class="gmap">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3170.0240064443615!2d-122.03229049909561!3d37.389264541919225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb653cc1cb057%3A0x61703970e2124d73!2s498+N+Mathilda+Ave%2C+Sunnyvale%2C+CA+94085!5e0!3m2!1sen!2sus!4v1545373108937" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    <ul>
+                    <p class="text">Open 7 Days: 8:30 AM - 6 PM</p>
+                        <li>498 N. Mathilda Avenue, Sunnyvale, CA 94085</li>
+                        <li>Phone: (408) 245 - 2122</li>
+                        <li>Fax:(408) 245 - 1211</li>
+                    </ul>
+                    
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- <script src='./Components/JS/api.js'></script> -->
+<?php include './Components/footer.php';?>
